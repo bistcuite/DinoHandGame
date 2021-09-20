12e4257a576051994c972ce58ffeb7b4992f496c
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-09-20 14:34:30 +0430

    Update main.py

--- main.py
+++ main.py
@@ -20,42 +20,38 @@ def fingerPosition(image, handNo=0):
     return lmList
 
 # Get input from webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("film.mp4")
 with mp_hands.Hands(
     min_detection_confidence=0.8,
     min_tracking_confidence=0.5) as hands:
     
-  # Get current frame
-  while cap.isOpened():
-    ret, image = cap.read()
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
-    results = hands.process(image)
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        # Draw landmark on image
-        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    # Save fingers position landmarks in a list
-    lmList = fingerPosition(image)
-    if len(lmList) != 0:
-        fingers = []
-        for id in range(1, 5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(1)
-            if (lmList[tipIds[id]][2] > lmList[tipIds[id] - 2][2] ):
-                fingers.append(0)
-        totalFingers = fingers.count(1)
-        
-        # if hand fisted, press space key
-        if totalFingers == 0:
-            pyautogui.press('space')
-            print("Space")
+    # Get current frame
+    while cap.isOpened():
+        ret, image = cap.read()
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    cv2.imshow("Hand Landmark", image)
-    cv2.waitKey(1)
-  cv2.destroyAllWindows()
+        # Save fingers position landmarks in a list
+        lmList = fingerPosition(image)
+        if len(lmList) != 0:
+            fingers = []
+            for id in range(1, 5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                if (lmList[tipIds[id]][2] > lmList[tipIds[id] - 2][2] ):
+                    fingers.append(0)
+            totalFingers = fingers.count(1)
+        
+            # if hand fisted, press space key
+            if totalFingers == 0:
+                pyautogui.press('space')
+
+        cv2.imshow("Hand Landmark", image)
+        cv2.waitKey(1)
+    cv2.destroyAllWindows()
